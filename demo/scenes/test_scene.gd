8413b7cf43b30e0d4c1158f2caa214ae8e12ee11
eweninja <97000698+mativizo@users.noreply.github.com>
2024-03-05 12:06:25 +0100

    Added simple example, added vosk_model_loaded signal

--- demo/scenes/test_scene.gd
+++ demo/scenes/test_scene.gd
@@ -1,12 +1,18 @@
-extends Node2D
+extends Control
 
 @onready var vosk_voice_recognition : VoskVoiceRecognition = $VoskVoiceRecognition
+@onready var loaded_model_label : Label = $MainMarginContainer/HBoxColumns/VBoxLeftColumn/LoadedModel
 
 func _ready():
 	vosk_voice_recognition.vosk_ready_signal.connect(_on_vosk_ready)
-	vosk_voice_recognition.init("x", 16000.0)
+	vosk_voice_recognition.vosk_model_loaded.connect(_on_vosk_model_loaded)
 	
-func _on_vosk_ready(is_ready :bool):
-	if is_ready: print("Vosk is ready.")
-	else: print("Vosk not ready")
+	vosk_voice_recognition.setLogLevel(2)
+	vosk_voice_recognition.init("bin/models/vosk-model-small-en-us-0.15")
 	
+func _on_vosk_ready(is_ready : bool, message : String):
+	if is_ready: print("Vosk is ready. Message: "+str(message))
+	else: print("Vosk not ready. Message: " + str(message))
+	
+func _on_vosk_model_loaded(model_path : String):
+	loaded_model_label.text = "Loaded model: "+str(model_path)
